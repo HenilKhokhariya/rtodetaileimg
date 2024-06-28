c0b610a51edd2896be16f25633c3bfa984fec85f
Henil Khokhariya <henilkhokhariya27@gmail.com>
2024-06-28 13:40:32 +0530

    wsq

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use(cors(corsObject));
 app.use("/api", router);
 app.listen(process.env.PORT, (res) => {
-  console.log("Server is running on port " + PORT);
+  console.log("Server is running on port " + process.env.PORT);
 });
