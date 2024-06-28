46e0753c24736914cbf9d4245011b97d6d5063e8
Henil Khokhariya <henilkhokhariya27@gmail.com>
2024-06-28 14:18:42 +0530

    dqwdwefwe

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const express = require("express");
-const router = require("./router");
-const cors = require("cors");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+let data = [];
 const corsObject = {
-  origin: "https://rtodetaileimg.onrender.com",
+  origin: "http://localhost:3000",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
   methods: "POST,GET,PUT,DELETE",
 };
-const app = express();
-
-app.use(express.json());
 app.use(cors(corsObject));
-app.use("/api", router);
-app.listen(process.env.PORT, (res) => {
-  console.log("Server is running on port " + process.env.PORT);
+app.use(express.json());
+
+app.post("/api/data", async (req, res) => {
+  try {
+    var img = "";
+    const vNumber = await req.body.vnumber;
+    await axios
+      .get(`https://www.carinfo.app/rc-details/${vNumber}`)
+      .then(async (res) => {
+        let $ = cheerio.load(res.data);
+
+        // $("div .css-yd8sa2").each(function (v, i) {
+        //   const Name = $(this).find("div .css-1tay05u p").text();
+        //   console.log(Name);
+        // });
+
+        $("div .css-11gihrn").each(function (index, el) {
+          img = $(this).find("img").attr("src");
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    res.status(200).json({ img: img });
+  } catch (error) {
+    console.log(data);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server is Start");
 });
--- router.js
+++
@@ -1,9 +0,0 @@
-const controler = require("./controler");
-const express = require("express");
-
-const router = express.Router();
-
-router.route("/").get(controler.Home);
-router.route("/data").post(controler.Data);
-
-module.exports = router;
